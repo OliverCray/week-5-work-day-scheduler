10ef4fef06ec081b5b7c7db0b614a47a1478a458
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-05-12 15:51:17 +0100

    Created and implemented displayDate function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
   // Store start and end work hours
   var workHours = {
     startHour: 9,
@@ -55,6 +36,9 @@ $(document).ready(function () {
   // Assign elements with class of saveBtn to the variable saveButton
   var saveButton = $('.saveBtn')
 
+  // Assign space in header where date and time will be displayed to the variable currentDay
+  var currentDay = $('#currentDay')
+
   // Saves event into local storage using data-hour as a key
   function onSaveClick() {
     var hour = $(this).parent().attr('data-hour')
@@ -85,10 +69,21 @@ $(document).ready(function () {
     })
   }
 
+  // Use dayjs to get current date and time and append to space in header
+  function displayDate() {
+    var currentDate = dayjs().format('DD MMM, YYYY [at] hh:mm:ss a');
+    currentDay.text(currentDate)
+  }
+
   // Call applyClass function
   applyClass()
   // Run applyClass function every 10 seconds to make sure classes are up to date
   setInterval(applyClass, 10000)
+
+  // Call displayDate function
+  displayDate();
+  // Run displayDate function every second to constantly update the date and time at the top of the page
+  setInterval(displayDate, 1000);
 
   // Event listener for clicks on save button, will execute onSaveClick function
   saveButton.on('click', onSaveClick)
